0c10cfc51fab8f65456fc7c7c1b4bf15b269f2d1
Wilfried Kakou <mianquihin15@gmail.com>
2019-05-31 23:01:22 +0000

    Adding files.

---
+++ src/components/data.js
@@ -0,0 +1,36 @@
+const data =
+{
+  artist_name:
+    [
+      { "id": 0, "text": 'Carl Doukouré' },
+    ],
+  art_name:
+    [
+      { "id": 0, "text": 'guerrier 4.0' },
+    ],
+  description:
+    [
+      { "id": 0, "text": 'consectetur adipisicing elit, sed do eiusmod tempor incididunt.' },
+    ],
+  tags: [
+    { "id": 0, "text": '#dark' },
+    { "id": 1, "text": '#afrofutirsm' },
+    { "id": 2, "text": '#black_people' }
+  ],
+  oeuvres:
+    [
+      { "id": 0, "text": './assets/static/images/oeuvres.png' }
+    ],
+  artefact: [
+    { "id": 0, "text": './assets/static/images/vues.png' },
+  ],
+  images_bottom:
+    [
+      { "id": 0, "text": './assets/static/images/likes.png' },
+      { "id": 1, "text": './assets/static/images/capture.png' },
+      { "id": 2, "text": './assets/static/images/comments.png' },
+    ],
+}
+
+
+export default data;
--- src/main.js
+++ src/main.js
@@ -1,5 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
+import BootstrapVue from 'bootstrap-vue/dist/bootstrap-vue.esm';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
+
+Vue.use(BootstrapVue);
+
 
 import VueQrcodeReader from "vue-qrcode-reader";
  
