d5f252fb2f1ca5c0b111574bf8d6895896225a09
Wilfried Kakou <mianquihin15@gmail.com>
2019-06-02 21:47:16 +0000

    Adding files.

---
+++ src/router/routes.js
@@ -0,0 +1,10 @@
+//import App from '@/App.vue'
+import Oeuvres from '@/components/Oeuvres.vue'
+import Home from '@/components/Home.vue'
+
+const routes = [
+  { path: '/', component: Home },
+  { path: '/oeuvres', component: Oeuvres },
+];
+
+export default routes;
