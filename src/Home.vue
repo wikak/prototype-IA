21ac2efaddfc21cbfe0f154e9378c671db0bfd62
Wilfried Kakou <mianquihin15@gmail.com>
2019-06-05 13:28:56 +0000

    Adding files.

---
+++ src/Home.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="home">
+      <qrcode />
+  </div>
+</template>
+
+<script>
+/* eslint-disable */
+import qrcode from '@/qr-code'
+export default {
+  name: "Home",
+  components: {
+    'qrcode': qrcode,
+  },
+};
+</script>
+
+<style>
+#home {
+    margin: 50px 0;
+    text-align: center
+}
+</style>
